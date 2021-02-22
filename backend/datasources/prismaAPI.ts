0ffd72f2edc22d755b3bdc459d943f009202d30a
Alvin Delito <alvindelito@live.com>
2021-02-22 13:20:33 -0800

    Add password to user role and hash using bcrypt

--- backend/datasources/prismaAPI.ts
+++ backend/datasources/prismaAPI.ts
@@ -1,4 +1,5 @@
 import { DataSource } from "apollo-datasource"
+import bcrypt from 'bcrypt';
 
 export default class prismaAPI extends DataSource {
   prisma: any;
@@ -11,6 +12,19 @@ export default class prismaAPI extends DataSource {
 
   initialize(config) {
     this.context = config.context
+  }
+
+  // USER LOGIN
+  async loginUser({ data: { email, password }}: any) {
+    // get hashed password from db
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: email
+      }
+    });
+
+    const result = await bcrypt.compare(password, user.password)
+    return await result ? user : 'incorrect field';
   }
 
   // GET INFO
@@ -65,11 +79,15 @@ export default class prismaAPI extends DataSource {
   }
 
   // CREATE USER & POST
-  async createUser({ data: { email, name, bio }}: any) {
+  async createUser({ data: { email, password, name, bio }}: any) {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     return await this.prisma.user.create({
       data: {
         email,
         name,
+        password: hashedPassword,
         profile: {
           create: {
             bio
