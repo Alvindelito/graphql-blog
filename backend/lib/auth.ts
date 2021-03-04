31142535303c34efeef78b4e3ac77f7342048deb
Alvin Delito <alvindelito@live.com>
2021-03-04 14:17:14 -0800

    changed jwt expiry time back to normal

--- backend/lib/auth.ts
+++ backend/lib/auth.ts
@@ -3,12 +3,12 @@ import { sign } from "jsonwebtoken"
 // TODO: fix 'any' type of user
 export const createAccessToken = (user: any) => {
   return sign({ userId: user.id }, process.env.ACCESS_TOKEN_SECRET!, {
-    expiresIn: '5s'
+    expiresIn: '15m'
   })
 }
 
 export const createRefreshToken = (user: any) => {
   return sign({ userId: user.id, tokenVersion: user.tokenVersion }, process.env.REFRESH_TOKEN_SECRET!, {
-    expiresIn: '15s'
+    expiresIn: '7d'
   })
 }
